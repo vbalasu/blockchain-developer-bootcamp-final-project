3d6771a566074ddc93b3053b62840b827553e096
Vijay Balasubramaniam <vbalasu@Nithins-MacBook-Pro.local>
2022-01-15 17:56:24 -0800

    manageWallet.js to fund test transactions

---
+++ front-end/manageWallet.js
@@ -0,0 +1,16 @@
+var Web3 = require('web3');
+var web3 = new Web3('http://localhost:7545');
+async function getBalance(address) {
+    var balance = await web3.eth.getBalance(address);
+    return web3.utils.fromWei(balance, 'ether');
+}
+async function send5eth() {
+    await web3.eth.sendTransaction({from: '0x353A375d1EbAbd1FdBd30bC07D08A7cB4EdC52E0', to: '0x539BA486B9C2ea933814B6310eE46A96bE3990eA', value: web3.utils.toWei('5', 'ether')});
+    return await getBalance('0x539BA486B9C2ea933814B6310eE46A96bE3990eA');
+}
+let ethBalance;
+async function main() {
+    ethBalance = await getBalance('0x539BA486B9C2ea933814B6310eE46A96bE3990eA');
+    console.log(ethBalance);
+}
+main()
